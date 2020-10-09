186a977044faf88524f600b5240ee88acaffeab8
Patrick Oliver <poliver24@hotmail.com>
2020-10-09 21:23:54 +0100

    Header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
-// import { Nav, Form, FormControl, Button } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Form from "react-bootstrap/Form";
@@ -14,16 +11,16 @@ export default function Header() {
   return (
     <div>
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand>
+          <a href="#">
+            <img src={logo} style={{ width: 100, marginTop: -7 }} />
+          </a>
+        </Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#features">Features</Nav.Link>
           <Nav.Link href="#pricing">Pricing</Nav.Link>
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
       </Navbar>
     </div>
   );
