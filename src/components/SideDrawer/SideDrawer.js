a7259547606a4769928ced714e683a6f34f20d5a
Patrick Oliver <poliver24@hotmail.com>
2020-12-10 22:41:43 +0000

    Updated SideDrawer Content

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
-import clsx from "clsx";
-import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
-import DrawerAppContent from '@material/react-drawer'
-import Button from "@material-ui/core/Button";
+
 import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
+import EventIcon from "@material-ui/icons/Event";
+import PeopleIcon from "@material-ui/icons/People";
+import EmailIcon from "@material-ui/icons/Email";
+
+
+import InfoIcon from "@material-ui/icons/Info";
+
 import "@material/drawer/dist/mdc.drawer.css";
 import { OpenInNewRounded } from "@material-ui/icons";
 
@@ -19,30 +21,41 @@ class sideDrawer extends Component {
   render() {
     return (
       <div>
-        <Drawer
-          modal
-          open="true"
-         >
+        <Drawer modal open="true">
           <List>
-            {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
+            <ListItem button key="ABOUT">
+              <ListItemIcon>
+                <InfoIcon />
+              </ListItemIcon>
+              <ListItemText primary="ABOUT" />
+            </ListItem>
           </List>
           <Divider />
           <List>
-            {["All mail", "Trash", "Spam"].map((text, index) => (
-              <ListItem button key={text}>
+            <ListItem button key="TEAM">
+              <ListItemIcon>
+                <PeopleIcon />
+              </ListItemIcon>
+              <ListItemText primary="TEAM" />
+            </ListItem>
+          </List>
+          <Divider />
+          <List>
+            <ListItem button key="BOOK">
+              <ListItemIcon>
+                <EventIcon />
+              </ListItemIcon>
+              <ListItemText primary="BOOK" />
+            </ListItem>
+          </List>
+          <Divider />
+          <List>
+              <ListItem button key="CONTACT">
                 <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  <EmailIcon />
                 </ListItemIcon>
-                <ListItemText primary={text} />
+                <ListItemText primary="CONTACT" />
               </ListItem>
-            ))}
           </List>
         </Drawer>
       </div>
